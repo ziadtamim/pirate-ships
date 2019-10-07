d7c928925cf1cc7b8b633664b0fe719e5772e72a
ziad <hey@byziad.com>
2019-10-07 10:45:00 +0200

    Initial commit

---
+++ PirateShips/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PirateShips
+//
+//  Created by Ziad Tamim on 07.10.19.
+//  Copyright © 2019 ByZiad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
